4599d2913367a9695442b2793eae469d151baf3a
kasra paketeghad <kasrapak69@gmail.com>
2022-10-16 12:14:15 +0330

    webpack init

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path')
+const webpack = require('webpack')
+
+const devServerPort = 3000
+
+module.exports = {
+  entry: "./src/index.js",
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules)/,
+        loader: "babel-loader",
+        options: {presets: ["@babel/preset-env"]}
+      }
+    ]
+  },
+  resolve: {extensions : ["*", ".js", ".jsx"]},
+  output: {
+    path: path.resolve(__dirname, "dist/"),
+    publicPath: "/dist/",
+    filename: "bundle.js"
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+      publicPath: `http://localhost:${devServerPort}/dist/`
+    },
+    // contentBase: path.join(__dirname, "public/"),
+    port: devServerPort,
+    open: {
+      app: {
+        name: 'firefox',
+      }
+    },
+    // publicPath: `http://localhost:${devServerPort}/dist/`,
+    hot: true,
+  },
+
+}
