1aeaa72d74158dfc09406d7239d3c1d4fb3b7bd7
kasra paketeghad <kasrapak69@gmail.com>
2022-12-12 13:12:22 +0330

    Eslint prop-types rule disabled

--- src/assets/styles/theme.jsx
+++ src/assets/styles/theme.jsx
@@ -1,27 +1,28 @@
-import React from 'react';
+import React from "react";
 import { ThemeProvider } from "styled-components";
 
 const theme = {
   general: {
-    color1: 'hsl(220, 98%, 61%)',
-    color2: 'linear-gradient(to right bottom, hsl(192, 100%, 67%), hsl(280, 87%, 65%))',
+    color1: "hsl(220, 98%, 61%)",
+    color2:
+      "linear-gradient(to right bottom, hsl(192, 100%, 67%), hsl(280, 87%, 65%))",
   },
   light: {
-    color1: 'hsl(0, 0%, 98%)',
-    color2: 'hsl(236, 33%, 92%)',
-    color3: 'hsl(233, 11%, 84%)',
-    color4: 'hsl(236, 9%, 61%)',
-    color5: 'hsl(235, 19%, 35%)',
+    color1: "hsl(0, 0%, 98%)",
+    color2: "hsl(236, 33%, 92%)",
+    color3: "hsl(233, 11%, 84%)",
+    color4: "hsl(236, 9%, 61%)",
+    color5: "hsl(235, 19%, 35%)",
   },
   dark: {
-    color1: 'hsl(235, 21%, 11%)',
-    color2: 'hsl(235, 24%, 19%)',
-    color3: 'hsl(234, 39%, 85%)',
-    color4: 'hsl(236, 33%, 92%)',
-    color5: 'hsl(234, 11%, 52%)',
-    color6: 'hsl(233, 14%, 35%)',
-    color7: 'hsl(237, 14%, 26%)',
-  }
+    color1: "hsl(235, 21%, 11%)",
+    color2: "hsl(235, 24%, 19%)",
+    color3: "hsl(234, 39%, 85%)",
+    color4: "hsl(236, 33%, 92%)",
+    color5: "hsl(234, 11%, 52%)",
+    color6: "hsl(233, 14%, 35%)",
+    color7: "hsl(237, 14%, 26%)",
+  },
 };
 
 // --Bright - Blue: hsl(220, 98 %, 61 %);
