c84e37cd71098a2a40c5bffeb167496af7554b8f
kasra paketeghad <kasrapak69@gmail.com>
2023-01-04 11:14:03 +0330

    Redux store setup

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { Provider } from 'react-redux';
 import { ThemeProvider } from "styled-components";
-import App from "./App";
+
+import store from './store';
 import { theme } from "@assets/styles/theme";
+import App from "./App";
 
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>
 );
